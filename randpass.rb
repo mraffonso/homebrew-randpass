5442d0c0c0f21fdfc7bce2c71b0b32198ba061af
Mario Affonso <mario@thecodebeach.com>
2018-03-31 17:36:37 -0400

    Try to fix formulae with new binary

--- randpass.rb
+++ randpass.rb
@@ -1,8 +1,8 @@
 class Randpass < Formula
   desc "Adequate command line random password generator for macOS"
   homepage "https://github.com/mraffonso/randpass"
-  url "https://github.com/mraffonso/randpass/releases/download/v0.1.0/randpass-0.1.0-darwin-amd64.zip"
-  sha256 "52e840b3335838cc37c4c893cc90fae1d0a189bd1d5abe6465681a03a5d63291"
+  url "https://github.com/mraffonso/randpass/releases/download/v0.1.0/randpass-0.1.0-darwin-amd64.tar.xz"
+  sha256 "4f1400bfe954492ce117fefd7763a4f2f05367f7b4e4578a695c38c9d409c734"
   version '0.1.0'
 
   def install
