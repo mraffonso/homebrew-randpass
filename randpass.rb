892f2a35a1703737010f7f8d316c97fa03e2a3af
Mario Affonso <mario@thecodebeach.com>
2018-08-18 16:57:01 -0400

    Changed for revert to github

--- randpass.rb
+++ randpass.rb
@@ -1,7 +1,7 @@
 class Randpass < Formula
-  desc "Adequate command line random password generator for macOS"
-  homepage "https://bitbucket.org/mraffonso/randpass"
-  url "https://bitbucket.org/mraffonso/randpass/downloads/randpass-0.1.3-darwin-amd64.tar.xz"
+  desc "An adequate command-line random password generator for macOS"
+  homepage "https://mraffonso.github.io/randpass/"
+  url "https://github.com/mraffonso/randpass/releases/download/v0.1.3/randpass-0.1.3-darwin-amd64.tar.xz"
   sha256 "558a780ad182a79b92ca124c61331ef93b9a56f9733e48c31202d005870c271f"
   version '0.1.3'
 
@@ -14,7 +14,7 @@ class Randpass < Formula
   test do
     require "open3"
     randpass_bin = "#{bin}/randpass"
-    ::Open3.popen3("#{puma_dev_bin} -h") do |_, stdout, _|
+    ::Open3.popen3("#{randpass_bin} -h") do |_, stdout, _|
       assert_equal "Randpass 0.1.3", stdout.readlines.first.strip
     end
   end
