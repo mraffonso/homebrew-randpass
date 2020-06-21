5a998ab77fb6e652dd831409d8a5d8f3e3067aa3
Mario Affonso <mario@thecodebeach.com>
2020-06-20 22:31:42 -0400

    Bump version to 0.1.5

--- randpass.rb
+++ randpass.rb
@@ -1,10 +1,11 @@
 class Randpass < Formula
   desc "An adequate command-line random password generator for macOS"
   homepage "https://mraffonso.github.io/randpass/"
-  url "https://github.com/mraffonso/randpass/releases/download/v0.1.4/randpass-0.1.4-darwin-amd64.tar.xz"
-  sha256 "050bb4d8c5613deb05b10f5d649a1246b09d4353355dd69a37923775fad254ba"
-  version '0.1.4'
+  url "https://github.com/mraffonso/randpass/releases/download/v0.1.5/randpass-0.1.5-darwin-amd64.tar.xz"
+  sha256 "507e2eb446fa98578b188d2c4158870afa9ffe5c846d1a54da843742ae0cc41a"
+  version '0.1.5'
 
+  depends_on "jq"
   depends_on "libevent"
 
   def install
@@ -15,7 +16,7 @@ class Randpass < Formula
     require "open3"
     randpass_bin = "#{bin}/randpass"
     ::Open3.popen3("#{randpass_bin} -h") do |_, stdout, _|
-      assert_equal "Randpass 0.1.4", stdout.readlines.first.strip
+      assert_equal "Randpass 0.1.5", stdout.readlines.first.strip
     end
   end
 end
