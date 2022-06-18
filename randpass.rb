3bb11ef4de37da89adfc332f0ebbe5319fe4f57d
Mario Affonso <mario@thecodebeach.com>
2022-06-18 10:51:59 -0400

    Revert homepage to github.io page

--- randpass.rb
+++ randpass.rb
@@ -1,6 +1,6 @@
 class Randpass < Formula
   desc "An adequate command-line random password generator"
-  homepage "https://github.com/mraffonso/randpass"
+  homepage "https://mraffonso.github.io/randpass"
   url "https://github.com/mraffonso/randpass/releases/download/v0.1.6/randpass-0.1.6-darwin-amd64.tar.xz"
   sha256 "a16e94c9263ed57a0678321c88f6cc1f284000989796d236d96dece7912b3fce"
   version '0.1.6'
